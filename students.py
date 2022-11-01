b9e559f23eb03afa44b3611f7ea4092506f6401f
Swetha-prasad <swethaprasad6235@gmail.com>
2022-11-01 16:16:29 +0530

    created connection

---
+++ students.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+mydb=mysql.connector.connect(host = 'localhost', user = 'root' , password = '' ,database= 'studentdb')
+
+mycursor=mydb.cursor()
+while True:
+    print("select an option from the menu")
+    print('1 add student')
+    print('2 view all students')
+    print('3 search a student')
+    print('4 update the student')
+    print('5 delete a student')
+    print('6 exit')
+
+    choice =int(input('enter an option: '))
+    if(choice==1):
+        print('student enter selected')
+        name = input('enter the name')
+        admno = input('enter the admissionnumber')
+        college = input('enter the college name')
+        rollnumber = input('enter the rollnumber')
+        sql ='INSERT INTO `students`(`name`, `rollnumber`, `admno`, `college`) VALUES (%s,%s,%s,%s)'
+        data = (name,rollnumber,admno,college)
+        mycursor.execute(sql,data)
+        mydb.commit()
+
+    elif(choice==2):
+        print('view student')  
+    elif(choice==3):
+        print('search a student')  
+    elif(choice==4):
+        print('update the student')
+    elif(choice==5):
+        print('delete the student') 
+    elif(choice==6):
+        break               
